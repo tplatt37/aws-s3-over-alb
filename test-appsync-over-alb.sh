4027f2ec1efacf90b5cb2aaf18e9a505a26b744b
Tim Platt <timothy.r.platt@gmail.com>
2026-02-26 17:28:04 -0500

    might work - needs testing

--- test-appsync-over-alb.sh
+++ test-appsync-over-alb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This is an introspection query - Introspection is on by default in AppSync
+# If you get "cloud not locate private api" your NGINX is not configured properly
 curl -X POST https://appsync.dev.awstrainer.com/graphql \
   -H "Content-Type: application/json" \
   -H "x-api-key: $1" \
